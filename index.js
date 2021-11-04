00ad738346b3096d5c1c4d331d343b5380921655
AnthonyEdwardStark3000 <90055674+AnthonyEdwardStark3000@users.noreply.github.com>
2021-11-04 09:31:21 +0530

    Done with express js basics and tbh its more easier than spring

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+const { json } = require('express');
 const express =require('express'); //leftside expess is just an variable
 const path = require('path');//js module to deal with paths
 const app = express(); //express() is the name of the first variable that contains the methods of imported express class
 const uuid=require('uuid');
-
+const exphs = require('express-handlebars');//#handle
 // const moment =require('moment');//to print the current time install  { npm i moment }
 
 const members = require('./Members');// importing the hardcoded js object for api testing
 const middleware=require('./middlewarevariable/middlewareVariable');//importing the created middleware
 
+//handlebars middleware #handle
+app.engine('handlebars',exphs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//creating route #handlebars //Homepage route index.handlebars
+app.get('/api/members',function(req,res)
+{
+    res.render('index' ,{
+        content:'This second parameter is optional it is used to pass information',
+        // members: members, (or)
+        members
+    });
+});
 
 // *******************************************************************************************
 // To use static folder here public is made static
@@ -15,6 +29,8 @@ const middleware=require('./middlewarevariable/middlewareVariable');//importing 
 // app.use(express.static(path.join(__dirname,'public')));
 
 // this folder will be loaded by default instead of sendFile
+
+//comment the handlebars route #handlebars and uncomment here inorder to see the change.
 
 // *******************************************************************************************
 
@@ -59,7 +75,7 @@ const middleware=require('./middlewarevariable/middlewareVariable');//importing 
 // 3)Trying to get only an single element by its id
     app.get('/api/members/:id',function(req,res){//if placed in another file express should be imported and router (i.e) const router =express.Router(); router.get() instead of app.get()
 
-        const found= members.some(member=>member.id=== parseInt(req.params.id));//the code runs and gives an element even without this condition but it returns[] when an request to access non existing member in the hardcoded js object.Inorder to make it return an proper messge this line is used and the condition to return the element is wrapped inside the if block. 
+        const found= members.some(member=>member.id === parseInt(req.params.id));//the code runs and gives an element even without this condition but it returns[] when an request to access non existing member in the hardcoded js object.Inorder to make it return an proper messge this line is used and the condition to return the element is wrapped inside the if block. 
 
         if(found)
         {
@@ -102,10 +118,68 @@ const middleware=require('./middlewarevariable/middlewareVariable');//importing 
         else // else is not included it will throw an headers already sent error
         {
             members.push(newMember);//if all the details are entered correctly I want to push the array into the hardcoded JS object.
-            res.json(members);
+            
+            // res.json(members); commenting this line now as while working with handlebar templates
+            // require to redirect and this returns the result in json array format
+
+            res.redirect('/api/members');
         }
         
     });
+
+// *******************************************************************************************
+
+
+// *******************************************************************************************
+    //  5)    update
+    app.put('/api/members/:id',function(req,res)
+    {
+        const found=members.some(member=>member.id === parseInt(req.params.id));
+
+        if(found)
+        {
+           const updtMember=req.body;
+           members.forEach(member=> 
+            {
+                if(member.id === parseInt(req.params.id))
+                {
+                    member.name=updtMember.name ? updtMember.name : member.name;
+                    member.age=updtMember.age ? updtMember.age : member.age;
+                    member.email=updtMember.email ? updtMember.email : member.email;
+                    res.json({msg:'Updated the details',member})
+                }
+            });
+        }
+        else{
+            res.status(400).json({msg:'No member with the id of '+`${req.params.id}`});
+        }
+    })
+// *******************************************************************************************
+// *******************************************************************************************
+    //  6)    Delete
+    app.delete('/api/members/:id',function(req,res)
+    {
+        const found=members.some(member=>member.id === parseInt(req.params.id));
+
+        if(found)
+        {
+           res.json({
+               msg:'member deleted id'+`${req.params.id}`,
+               members:members.filter(member =>member.id !== parseInt(req.params.id))
+           });
+        }
+        else{
+            res.status(400).json({msg:'No member with the id of '+`${req.params.id}`});
+        }
+    })
+// *******************************************************************************************
+
+// *******************************************************************************************
+    //               7)Rendering with template 1.install handlebars {npm i express-handlebars}
+    // import express-handlebars id='handle';
+    //create middleware
+    //create handle bars in folders views,layout. views->index.handlebars->layout->main.handlebars
+    //create routes to render id="handlebars"
 
 // *******************************************************************************************
 
